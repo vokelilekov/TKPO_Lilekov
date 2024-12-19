3eba2583e1cd68444ce36259c1297d92b3f19933
Вадим Лилеков <vokelilekov@gmail.com>
2024-12-19 21:29:09 +0300

    'State'

---
+++ State/State/MainForm.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace State
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCall = new System.Windows.Forms.Button();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.btnRecharge = new System.Windows.Forms.Button();
+            this.lblState = new System.Windows.Forms.Label();
+            this.lblBalance = new System.Windows.Forms.Label();
+            this.txtRechargeAmount = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCall
+            // 
+            this.btnCall.Location = new System.Drawing.Point(249, 205);
+            this.btnCall.Name = "btnCall";
+            this.btnCall.Size = new System.Drawing.Size(148, 60);
+            this.btnCall.TabIndex = 0;
+            this.btnCall.Text = "CALL";
+            this.btnCall.UseVisualStyleBackColor = true;
+            this.btnCall.Click += new System.EventHandler(this.btnCall_Click);
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(95, 205);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(148, 60);
+            this.btnAnswer.TabIndex = 1;
+            this.btnAnswer.Text = "ANSWER";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(403, 205);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(148, 60);
+            this.btnEnd.TabIndex = 2;
+            this.btnEnd.Text = "END";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // btnRecharge
+            // 
+            this.btnRecharge.Location = new System.Drawing.Point(249, 378);
+            this.btnRecharge.Name = "btnRecharge";
+            this.btnRecharge.Size = new System.Drawing.Size(148, 60);
+            this.btnRecharge.TabIndex = 3;
+            this.btnRecharge.Text = "RECHARGE";
+            this.btnRecharge.UseVisualStyleBackColor = true;
+            this.btnRecharge.Click += new System.EventHandler(this.btnRecharge_Click);
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(55, 43);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(164, 20);
+            this.lblState.TabIndex = 4;
+            this.lblState.Text = "Текущее состояние: ";
+            // 
+            // lblBalance
+            // 
+            this.lblBalance.AutoSize = true;
+            this.lblBalance.Location = new System.Drawing.Point(246, 116);
+            this.lblBalance.Name = "lblBalance";
+            this.lblBalance.Size = new System.Drawing.Size(151, 20);
+            this.lblBalance.TabIndex = 5;
+            this.lblBalance.Text = "Баланс: 50 единиц";
+            // 
+            // txtRechargeAmount
+            // 
+            this.txtRechargeAmount.Location = new System.Drawing.Point(249, 322);
+            this.txtRechargeAmount.Name = "txtRechargeAmount";
+            this.txtRechargeAmount.Size = new System.Drawing.Size(148, 26);
+            this.txtRechargeAmount.TabIndex = 6;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 511);
+            this.Controls.Add(this.txtRechargeAmount);
+            this.Controls.Add(this.lblBalance);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.btnRecharge);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnAnswer);
+            this.Controls.Add(this.btnCall);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCall;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Button btnRecharge;
+        private System.Windows.Forms.Label lblState;
+        private System.Windows.Forms.Label lblBalance;
+        private System.Windows.Forms.TextBox txtRechargeAmount;
+    }
+}
+
---
+++ State/State/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using StatePatternPhoneApp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace State
+{
+    public partial class MainForm : Form
+    {
+        private Phone phone;
+        public MainForm()
+        {
+            InitializeComponent();
+            phone = new Phone("123-456-789", 50.0, 0.5);
+            UpdateStateLabel();
+        }
+
+        private void UpdateStateLabel()
+        {
+            lblState.Text = $"Текущее состояние: {phone.GetStateName()}"; 
+            lblBalance.Text = $"Баланс: {phone.Balance} единиц";  
+        }
+
+        private void btnCall_Click(object sender, EventArgs e)
+        {
+            phone.Call();
+            UpdateStateLabel();
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            phone.AnswerCall();
+            UpdateStateLabel();
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            phone.EndCall();
+            UpdateStateLabel();
+        }
+
+        private void btnRecharge_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtRechargeAmount.Text, out double amount))
+            {
+                phone.Recharge(amount);
+                UpdateStateLabel();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректную сумму для пополнения.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
